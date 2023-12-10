9cc06e5a43c34a4729933b9065b2f3ce7ccb3e53
Mozzammel Hossen <53122797+abirpuc@users.noreply.github.com>
2023-12-11 00:18:34 +0600

    design saving, loans and finacila advive card

--- src/Layout/mainContent/MainContent.jsx
+++ src/Layout/mainContent/MainContent.jsx
@@ -1,5 +1,7 @@
 import Budget from '../../components/Budget/Budget'
 import Card from '../../components/Card/Card'
+import Financial from '../../components/Financial/Financial'
+import Loans from '../../components/Loans/Loans'
 import Report from '../../components/Report/Report'
 import Saving from '../../components/Saving/Saving'
 import Subscription from '../../components/Subscription/Subscription'
@@ -8,15 +10,28 @@ import './maincontent.css'
 
 export default function MainContent() {
   return (
-    <div className='main-content-holder'>
-      <div className="content-grid-one">
-        <Card/>
-        <Transaction/>
-        <Report/>
-        <Budget/>
-        <Subscription/>
-        <Saving/>
-      </div>
+    <div className="main-content-holder">
+        <div className="content-grid-one">
+            <Card />
+            <Transaction />
+            <Report />
+        </div>
+        <div className="content-grid-two">
+            <Budget />
+            <div className="grid-two-item">
+              <div className="subgrid-two">
+                <Subscription />
+                <Saving />
+              </div>
+            </div>
+
+            <div className="grid-two-item">
+              <div className="subgrid-two">
+                <Loans />
+                <Financial />
+              </div>
+            </div>
+        </div>
     </div>
   )
 }
